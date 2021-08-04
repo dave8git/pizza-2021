989d79c6d3393478e43a7d3a494b75fc9a191e3f
dave8git <dave8git@gmail.com>
2021-08-04 04:14:32 -0700

    fix proble with product in the cart

---
+++ src/js/components/AmountWidget.js
@@ -0,0 +1,72 @@
+import {settings, select} from '../settings.js';
+
+class AmountWidget {
+  constructor(element) {
+    const thisWidget = this;
+
+    thisWidget.getElements(element);
+    thisWidget.setValue(settings.amountWidget.defaultValue); // aby właściwość thisWidget.setValue miała wartość początkową, i aby mógł wykonać się if
+    thisWidget.initActions();
+    // console.log('AmountWidget:', thisWidget);
+    // console.log('constructor arguments', element);
+  }
+  getElements(element) {
+    const thisWidget = this;
+
+    thisWidget.element = element;
+    //console.log('thisWidget.element', thisWidget.element);
+    thisWidget.input = thisWidget.element.querySelector(
+      select.widgets.amount.input
+    );
+    //console.log('thisWidget.input', thisWidget.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkDecrease
+    );
+    thisWidget.linkIncrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkIncrease
+    );
+  }
+  setValue(value) {
+    const thisWidget = this;
+
+    const newValue = parseInt(value);
+
+    /* TODO: Add validation */
+    if (
+      thisWidget.value !== newValue &&
+      !isNaN(newValue) &&
+      newValue >= settings.amountWidget.defaultMin &&
+      newValue <= settings.amountWidget.defaultMax
+    ) {
+      thisWidget.value = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.input.value = thisWidget.value;
+  }
+  initActions() {
+    const thisWidget = this;
+    thisWidget.input.addEventListener('change', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.input.value);
+    });
+    thisWidget.linkDecrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+    thisWidget.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+  announce() {
+    const thisWidget = this;
+    const event = new CustomEvent('updated', {
+      bubbles: true,
+    });
+
+    thisWidget.element.dispatchEvent(event);
+  }
+}
+
+export default AmountWidget;
