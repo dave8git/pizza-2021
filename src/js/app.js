aca65e6709127cf5816800eeb0834bd046f2fb2c
dave8git <dave8git@gmail.com>
2021-08-10 03:02:43 -0700

    add generated html and widgets for booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames, templates} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function () {
@@ -98,6 +99,12 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+  initBooking: function () {
+    const thisApp = this; 
+    const bookingElem = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(bookingElem);
+  },
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -110,6 +117,7 @@ const app = {
     thisApp.initData();
     thisApp.initMenu();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,33 @@
+import {select, classNames, templates} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+//import {utils} from '../utils.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this; //
+    thisBooking.render(element);
+    thisBooking.initWidgets(); 
+  }
+  render(element) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    //thisBooking.dom.elem = utils.createDOMFromHTML(generatedHTML);
+    //thisBooking.dom.wrapper = thisBooking.dom.element.querySelector(element);
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+  }
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(
+      thisBooking.dom.peopleAmount
+    );
+    thisBooking.hoursAmount = new AmountWidget(
+      thisBooking.dom.hoursAmount
+    );
+  }
+}
+
+export default Booking;
